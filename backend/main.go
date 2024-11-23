b10e440dc74e43c42054db879a3d6c72d616b8ba
Franta1205 <hoza.fr@gmail.com>
2024-11-23 17:38:02 +0100

    clean up

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"stageflow/app"
-	"stageflow/config/initializers"
 )
 
 func main() {
-	f := initializers.LoadEnvVariable("DB_USER")
-	fmt.Println(f)
 	app.StartApp()
 }
