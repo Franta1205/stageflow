b5f059a6d8bb5ed146430aa1e5f9e709556f7d13
Franta1205 <hoza.fr@gmail.com>
2024-11-23 17:04:20 +0100

    dtos

---
+++ backend/dtos/authentication.go
@@ -0,0 +1,13 @@
+package dtos
+
+type SignUpRequestDTO struct {
+	FirstName string `json:"firstName" binding:"required"`
+	LastName  string `json:"lastName" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required"`
+}
+
+type SignInRequestDTO struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
--- backend/models/user.go
+++ backend/models/user.go
@@ -1,11 +1,11 @@
-package user
+package models
 
 import "time"
 
 type User struct {
 	ID        uint   `json:"id" gorm:"primary_key"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 	Email     string `json:"email" gorm:"unique"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
