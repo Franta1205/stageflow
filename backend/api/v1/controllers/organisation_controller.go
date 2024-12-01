011bff22fe19bdcfc543768515c11bc38aef2321
Franta1205 <hoza.fr@gmail.com>
2024-12-01 15:03:39 +0100

    binding org

--- backend/api/v1/controllers/organisation_controller.go
+++ backend/api/v1/controllers/organisation_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"stageflow/api/v1/dto"
 )
 
 type OrganisationController struct{}
@@ -12,5 +13,10 @@ func NewOrganisationController() *OrganisationController {
 }
 
 func (oc *OrganisationController) Create(c *gin.Context) {
+	var organisationRequest dto.OrganisationRequest
+	if err := c.ShouldBindJSON(&organisationRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{"message": "org created"})
 }
