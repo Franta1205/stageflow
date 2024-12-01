2d0c72b6b73012a8ea5185eecb5fbd4f87561de5
Franta1205 <hoza.fr@gmail.com>
2024-12-01 17:19:21 +0100

    fix migration

--- backend/db/migrations/000003_create_user_organisation.up.sql
+++ backend/db/migrations/000003_create_user_organisation.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE user_organisations (
-                                    user_id VARCHAR(36) NOT NULL PRIMARY KEY ,
-                                    organisation_id VARCHAR(36) NOT NULL PRIMARY KEY ,
+                                    user_id VARCHAR(36) NOT NULL,
+                                    organisation_id VARCHAR(36) NOT NULL,
                                     role VARCHAR(50) NOT NULL DEFAULT 'owner',
                                     created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
                                     updated_at TIMESTAMP(3) NOT NULL,
                                     PRIMARY KEY (user_id, organisation_id),
                                     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-                                    FOREIGN KEY (organisation_id) REFERENCES organisations(id) ON DELETE CASCADE,
+                                    FOREIGN KEY (organisation_id) REFERENCES organisations(id) ON DELETE CASCADE
 );
