85733159667592c58a06b615e2c912828bcaa908
Franta1205 <hoza.fr@gmail.com>
2024-12-07 23:40:31 +0100

    models refactor

---
+++ backend/api/v1/models/models.go
@@ -0,0 +1,44 @@
+package models
+
+import "time"
+
+type User struct {
+	ID            string          `json:"id" gorm:"type:varchar(36);primary_key"`
+	FirstName     string          `json:"firstName" gorm:"type:varchar(255);not null"`
+	LastName      string          `json:"lastName" gorm:"type:varchar(255);not null"`
+	Email         string          `json:"email" gorm:"type:varchar(255);not null;unique"`
+	Password      string          `json:"-" gorm:"type:varchar(255);not null"`
+	Organisations []*Organisation `gorm:"many2many:user_organisations"`
+	CreatedAt     time.Time       `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt     time.Time       `json:"updatedAt" gorm:"type:timestamp(3);not null"`
+}
+
+type Organisation struct {
+	ID        string    `json:"id" gorm:"type:varchar(36);primary_key"`
+	Name      string    `json:"name" gorm:"type:varchar(255);not null"`
+	Users     []*User   `gorm:"many2many:user_organisations"`
+	Webinars  []Webinar `json:"webinars" gorm:"foreignKey:OrganisationID"`
+	CreatedAt time.Time `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `json:"updatedAt" gorm:"type:timestamp(3);not null"`
+}
+
+type UserOrganisation struct {
+	UserID         string    `json:"userId" gorm:"type:varchar(36);primary_key"`
+	OrganisationID string    `json:"organisationId" gorm:"type:varchar(36);primary_key"`
+	Role           string    `json:"role" gorm:"type:varchar(50);not null;default:'owner'"`
+	CreatedAt      time.Time `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time `json:"updatedAt" gorm:"type:timestamp(3);not null"`
+
+	User         User         `json:"user" gorm:"foreignKey:UserID"`
+	Organisation Organisation `json:"organization" gorm:"foreignKey:OrganizationID"`
+}
+
+type Webinar struct {
+	ID             string        `json:"id" gorm:"type:varchar(36);primary_key"`
+	Title          string        `json:"title" gorm:"type:varchar(255);not null"`
+	ScheduledAt    string        `json:"scheduledAt" gorm:"type:timestamp(3);not null"`
+	OrganisationID string        `json:"organisationId" gorm:"type:varchar(36);not null"`
+	Organisation   *Organisation `json:"organisation" gorm:"foreignKey:OrganisationID"`
+	CreatedAt      time.Time     `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time     `json:"updatedAt" gorm:"type:timestamp(3);not null"`
+}
