d4a5827266460672fd302785e9e7c5ade6fac2d2
Franta1205 <hoza.fr@gmail.com>
2024-12-08 13:11:55 +0100

    refactor models

--- backend/api/v1/models/models.go
+++ backend/api/v1/models/models.go
@@ -14,12 +14,12 @@ type User struct {
 }
 
 type Organisation struct {
-	ID        string    `json:"id" gorm:"type:varchar(36);primary_key"`
-	Name      string    `json:"name" gorm:"type:varchar(255);not null"`
-	Users     []*User   `gorm:"many2many:user_organisations"`
-	Webinars  []Webinar `json:"webinars" gorm:"foreignKey:OrganisationID"`
-	CreatedAt time.Time `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt time.Time `json:"updatedAt" gorm:"type:timestamp(3);not null"`
+	ID        string     `json:"id" gorm:"type:varchar(36);primary_key"`
+	Name      string     `json:"name" gorm:"type:varchar(255);not null"`
+	Users     []*User    `gorm:"many2many:user_organisations"`
+	Webinars  []*Webinar `json:"webinars" gorm:"foreignKey:OrganisationID"`
+	CreatedAt time.Time  `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time  `json:"updatedAt" gorm:"type:timestamp(3);not null"`
 }
 
 type UserOrganisation struct {
@@ -34,11 +34,23 @@ type UserOrganisation struct {
 }
 
 type Webinar struct {
-	ID             string        `json:"id" gorm:"type:varchar(36);primary_key"`
-	Title          string        `json:"title" gorm:"type:varchar(255);not null"`
-	ScheduledAt    string        `json:"scheduledAt" gorm:"type:timestamp(3);not null"`
-	OrganisationID string        `json:"organisationId" gorm:"type:varchar(36);not null"`
-	Organisation   *Organisation `json:"organisation" gorm:"foreignKey:OrganisationID"`
-	CreatedAt      time.Time     `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
-	UpdatedAt      time.Time     `json:"updatedAt" gorm:"type:timestamp(3);not null"`
+	ID             string          `json:"id" gorm:"type:varchar(36);primary_key"`
+	Title          string          `json:"title" gorm:"type:varchar(255);not null"`
+	ScheduledAt    string          `json:"scheduledAt" gorm:"type:timestamp(3);not null"`
+	OrganisationID string          `json:"organisationId" gorm:"type:varchar(36);not null"`
+	Organisation   *Organisation   `json:"organisation" gorm:"foreignKey:OrganisationID"`
+	Participants   []*Participants `json:"participants" gorm:"foreignKey:ParticipantID"`
+	CreatedAt      time.Time       `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt      time.Time       `json:"updatedAt" gorm:"type:timestamp(3);not null"`
+}
+
+type Participants struct {
+	ID        string    `json:"id" gorm:"type:varchar(36);primary_key"`
+	Email     string    `json:"email" gorm:"type:varchar(255);not null;"`
+	FirstName string    `json:"firstName" gorm:"type:varchar(255);not null"`
+	LastName  string    `json:"lastName" gorm:"type:varchar(255);not null"`
+	WebinarID string    `json:"WebinarId" gorm:"type:varchar(36);not null"`
+	Webinar   *Webinar  `json:"webinar" gorm:"foreignKey:WebinarID"`
+	CreatedAt time.Time `json:"createdAt" gorm:"type:timestamp(3);not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `json:"updatedAt" gorm:"type:timestamp(3);not null"`
 }
